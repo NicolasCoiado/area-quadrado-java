9ffbb26c8d6cdb6e18a73d8f535f1e1d310a61ad
Nicolas Mauricio Martins Coiado <48792614+NicolasCoiado@users.noreply.github.com>
2022-10-05 23:50:52 -0300

    Update AreaQuadrado.java

--- AreaQuadrado.java
+++ AreaQuadrado.java
@@ -3,7 +3,7 @@ public class AreaQuadrado {
     public static void main (String [] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o lado do quadrado:");
-        double lado = Double.parseDouble(scanner.next());
+        double lado = scanner.nextDouble();
         double area = lado*lado;
         System.out.println("O dobro da área do quadrado é igual a: "+ area*2+".");
     }
